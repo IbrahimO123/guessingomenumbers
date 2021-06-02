5b2e2913ce4dbc701e3f8b2d1542cb2f6dd56ac3
Ibrahim <80628436+IbrahimO123@users.noreply.github.com>
2021-06-02 08:36:29 +0100

    animation and scope learnin

---
+++ scriptjava.js
@@ -0,0 +1,29 @@
+var navyArmy = 300;
+var airForceArmy = 600;
+var policemen;
+
+function armyInNation(){
+    let solidiers = 600;
+    let cia = 800
+let totalArmy = navyArmy * airForceArmy * solidiers * cia;
+console.log(totalArmy);
+};
+armyInNation();
+
+console.log(airForceArmy);
+policemen = 500;
+
+ console.log(policemen)
+
+ let mumWillCall = () => {
+
+    let ibrahimPick = false
+    let temitopeWillPick = true
+    if ( ibrahimPick !== true) {
+        console.log(`Ibrahim is scared of Temitope Mum and that is ${temitopeWillPick}`)
+    }
+    else {
+        console.log(`Temitope Mum did not call Ibrahim and Temitope pick the call and that is ${temitopeWillPick}`)
+    }
+ };
+ mumWillCall();
